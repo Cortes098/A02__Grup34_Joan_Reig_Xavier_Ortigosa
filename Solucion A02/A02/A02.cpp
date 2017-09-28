9721860768fe3e709bd33f616f8a08674a2f506b
JOAN REIG BERGUA <j-reig@hotmail.com>
2017-09-28 11:33:48 +0200

    Subo

--- Solucion A02/A02/A02.cpp
+++ Solucion A02/A02/A02.cpp
@@ -2,10 +2,30 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+template <typename T>
+void fExample1(T, int paramInt) {
+	//Cuerpor de la funcion
+	paramInt = 1;
+}
+
+template <class C>
+C CExample2(T T1, T T2) {
+
+}
+
+template <typename T>
+struct node{
+	T info;
+	node* next{ this };
+};
 
 
 int main()
 {
+
+	node<int> un{ 12 };
     return 0;
 }
 
